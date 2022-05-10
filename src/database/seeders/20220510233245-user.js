125d430a009de91384e3e7d41e31388eace6c7a1
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-05-10 20:46:01 -0300

    feat: implementa seed para users

---
+++ src/database/seeders/20220510233245-user.js
@@ -0,0 +1,22 @@
+module.exports = {
+  async up(queryInterface, _Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+      name: 'ALLAN SOUZA',
+      email: 'allan@cashforce.com.br',
+      phoneNumber: null,
+      mobile: null,
+      departament: null,
+      verificationCode: '',
+      emailChecked: 1,
+      createdAt: '2020-10-01 21:31:37',
+      updatedAt: '2020-10-01 22:41:23',
+      cashforceAdm: 1
+    },
+  ], {});
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
