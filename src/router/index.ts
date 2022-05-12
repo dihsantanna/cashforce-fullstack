06d5135e7e61d4307f600e4242148eaa93b7828b
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-05-12 19:32:33 -0300

    feat: configura rotas

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,15 +4,14 @@ import InvoicesView from '../views/InvoicesView.vue';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'invoices',
-    component: InvoicesView,
+    redirect: 'invoices',
   },
   {
     path: '/invoices',
-    name: 'invoices',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    name: 'invoices',
     component: InvoicesView,
   },
 ];
