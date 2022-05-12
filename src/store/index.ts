d8945d7800e26f18531cb09c120035e5ac0e01f0
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-05-12 19:33:58 -0300

    feat: implementa store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,21 @@
+import { Invoice } from '@/types/Invoice.interface';
 import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    invoices: [] as Invoice[],
   },
   getters: {
+    getInvoices(state) {
+      return state.invoices;
+    },
+    getProviders: (state) => (id: number) => (
+      state.invoices.find((invoice) => invoice.id === id)
+    ),
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    setInvoices(state, payload: Invoice[]) {
+      state.invoices = [...payload];
+    },
   },
 });
