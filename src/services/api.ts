ec28fdedd17abfc31a2d6d7fe3604ec06f3b8bbd
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-05-12 23:24:43 -0300

    fix: corrige erro na utilização do axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.VUE_APP_BASE_URL ?? 'http://localhost:3001/',
 });
 
 export const requestData = async (endpoint:string) => {
-  const { data } = await axios.get(endpoint);
+  const { data } = await api.get(endpoint);
   return data;
 };
 
