2ef83ce0067ab3910b18d7b9eeb98167bcf17408
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-09-30 14:46:22 -0300

    fix: corrige erro na chamada de variavel de ambiente no frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import 'dotenv/config';
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL ?? 'http://localhost:3001/',
+  baseURL: process.env.VUE_APP_BASE_URL || 'http://localhost:3001/',
 });
 
 export const requestData = async (endpoint:string) => {
